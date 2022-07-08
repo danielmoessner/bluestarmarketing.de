9740948dcb9186e563ae5bef2b392274d86eaf89
Daniel Mössner <github@tortuga-webdesign.de>
2022-07-08 13:25:54 +0200

    fix error

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,11 +1,11 @@
-// import { Category } from "./types/category";
+import { Category } from "./types/category";
 import { makeSource } from "contentlayer/source-files";
-// import { Animal } from "./types/animal";
+import { Animal } from "./types/animal";
 import { Legal } from "./types/legal";
 import { Customer } from "./types/customer";
 
 export default makeSource({
   contentDirPath: "content",
   contentDirExclude: ["setting", "page", "category", "animal"],
-  documentTypes: [Legal, Customer],
+  documentTypes: [Legal, Customer, Animal, Category],
 });
