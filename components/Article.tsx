b3686cc36dae8417321a324c4efd243667351b59
Daniel Mössner <github@tortuga-webdesign.de>
2022-07-18 17:51:25 +0200

    mobile stuff

--- components/Article.tsx
+++ components/Article.tsx
@@ -8,11 +8,13 @@ function Component({ image, article, children }) {
       <div className="leading-[0px]">
         <Image {...image} alt="test" />
       </div>
-      <h2 className="mt-10 text-5xl font-bold">{article.title}</h2>
-      <div className="mt-3">
+      <h2 className="mt-5 text-2xl font-bold lg:mt-4 lg:text-3xl">
+        {article.title}
+      </h2>
+      <div className="mt-1 lg:mt-2">
         <ArticleMeta article={article} />
       </div>
-      <div className="mt-5">
+      <div className="mt-8">
         <div>{children}</div>
       </div>
     </div>
