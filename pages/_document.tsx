dbfecb7226972ca9bcc278eca71e5c793a537403
Daniel Mössner <github@tortuga-webdesign.de>
2022-07-19 10:52:48 +0200

    add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,31 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="de">
       <Head>
+        {/* font */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap"
           rel="stylesheet"
+        />
+        {/* google */}
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+              `,
+          }}
         />
       </Head>
       <body>
