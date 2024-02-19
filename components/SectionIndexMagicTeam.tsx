392c6c48a690c26ed6e70b1837c2b1104ca87735
Daniel Mössner <github@moesibert.de>
2024-02-19 20:32:00 +0100

    neue headline positionierung

--- components/SectionIndexMagicTeam.tsx
+++ components/SectionIndexMagicTeam.tsx
@@ -8,7 +8,7 @@ import ProseRaw from "./ProseRaw";
 
 function SectionImageTitleRoseTextButton({
   section,
-  padding = "pt-12 pb-16 lg:pb-20",
+  padding = "pt-16 pb-16 lg:pb-20",
 }) {
   return (
     <section className={padding}>
@@ -17,40 +17,41 @@ function SectionImageTitleRoseTextButton({
           <Animate>
             <div className="flex justify-center mb-10">
               <Heading element="h2" size="h1">
-                Make-it-happen
+                <span>Make-it-happen</span>
+                <div className="inline-block ml-3 w-8 h-8 bg-[url('/stern_klein.svg')]"></div>
+                <span className="ml-5 text-4xl font-rose md:text-6xl">Add</span>{" "}
+                value
+                <span className="ml-5 text-4xl font-rose md:text-6xl">
+                  &
+                </span>{" "}
+                growth
               </Heading>
             </div>
             <div className="grid gap-8 lg:grid-cols-2">
               <div className="flex flex-col justify-between pt-14">
                 <div className="">
-                  <h3 className="relative inline-block text-3xl md:text-5xl ">
-                    {/* <span className="ml-5 text-5xl font-rose md:text-8xl text-bsm-ocean">
-                      Your
-                    </span> */}
-                    Add value & growth
-                    <div className="absolute flex items-center justify-center mx-auto pointer-events-none md:top-2 top-4 -right-7 md:-right-10 lg:-right-12">
-                      <HeadingStar />
-                    </div>
-                  </h3>
+                  <h3 className="relative inline-block text-3xl md:text-5xl "></h3>
                   <div className="mt-4 md:mt-14">
                     <ProseRaw>
-                      <p>
-                        Meistere Komplexität, Krisen & Dauerbrenner
-                        funktionsübergreifend & auf allen Ebenen
-                      </p>
-                      <p>
-                        Überwinde Hindernisse, Widerstände und schleppende
-                        Zusammenarbeit
-                      </p>
-                      <p>
-                        Neue & integrale Lösungen für Dich, Dein Business und
-                        Dein Team in Co-Creation
-                      </p>
-                      <p>
-                        Mit WIR-Power in einer Absicht & gemeinsamen Zielen zu
-                        <b> mehrWERT & mehrWACHSTUM</b>
-                      </p>
-                      <p>Zukunft ist JETZT!</p>
+                      <ul>
+                        <li>
+                          Meistere Komplexität, Krisen & Dauerbrenner
+                          funktionsübergreifend & auf allen Ebenen
+                        </li>
+                        <li>
+                          Überwinde Hindernisse, Widerstände und schleppende
+                          Zusammenarbeit
+                        </li>
+                        <li>
+                          Neue & integrale Lösungen für Dich, Dein Business und
+                          Dein Team in Co-Creation
+                        </li>
+                        <li>
+                          Mit WIR-Power in einer Absicht & gemeinsamen Zielen zu
+                          <b> mehrWERT & mehrWACHSTUM</b>
+                        </li>
+                        <li>Zukunft ist JETZT!</li>
+                      </ul>
                     </ProseRaw>
                   </div>
                 </div>
@@ -62,7 +63,7 @@ function SectionImageTitleRoseTextButton({
               </div>
               <div>
                 <div className="flex justify-start">
-                  <h3 className="relative inline-block pt-10 mt-5 text-3xl md:text-5xl">
+                  <h3 className="relative inline-block pt-10 text-3xl md:text-5xl">
                     MehrWERT
                     <div className="absolute flex items-center justify-center mx-auto pointer-events-none md:top-14 lg:top-10 top-12 -right-7 md:-right-10 lg:-right-12">
                       <HeadingStar />
